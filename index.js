31deda923c92cbc39961ca0f219f86620547aedc
tnty <tnty.working@gmail.com>
2022-09-29 18:40:23 +0300

    fix: request body limit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors());
 
 app.use('/api', collectionRouter);
